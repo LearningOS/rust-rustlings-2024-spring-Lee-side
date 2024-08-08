63e8694f0c99f81ef4b41ac7acd88c5e69f790af
tangjian <tangjianwork@qq.com>
2024-08-08 20:28:30 +0800

    feat: quiz3.

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -18,16 +18,27 @@
 
 // I AM NOT DONE
 
+pub enum Grade {
+    Numerical(f32),
+    Alphabetical(String),
+}
+
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Grade,
     pub student_name: String,
     pub student_age: u8,
 }
 
 impl ReportCard {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        match self.grade {
+            Grade::Numerical(grade) => {
+                format!("{} ({}) - achieved a grade of {}", &self.student_name, &self.student_age, grade)
+            }
+            Grade::Alphabetical(ref grade) => {
+                format!("{} ({}) - achieved a grade of {}", &self.student_name, &self.student_age, grade)
+            }
+        }
     }
 }
 
@@ -38,7 +49,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Numerical(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -52,7 +63,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Alphabetical("A+".to_string()),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
