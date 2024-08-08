cb078895e5f4ed1ff22a2ff4baa945133615fbae
tangjian <tangjianwork@qq.com>
2024-08-08 20:14:37 +0800

    feat: generics and traits.

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,10 +8,17 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
+}
+
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut new_vec = self; // 获取 self 的可变拷贝
+        new_vec.push(String::from("Bar")); // 在向量末尾添加 "Bar"
+        new_vec // 返回修改后的向量
+    }
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
